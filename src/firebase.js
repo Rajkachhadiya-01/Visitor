fb133e4d11a8657e8df46650b96728096901aa04
Raj Kachhadiya <rajkachhadiya05@gmail.com>
2025-11-14 09:12:25 +0530

    safe

--- src/firebase.js
+++ src/firebase.js
@@ -10,14 +10,15 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAWQlfBOt8bVNtrFPq1QMhEglW04yQ2KIw",
-  authDomain: "gatepass-15b35.firebaseapp.com",
-  projectId: "gatepass-15b35",
-  storageBucket: "gatepass-15b35.firebasestorage.app",
-  messagingSenderId: "136754359619",
-  appId: "1:136754359619:web:0a23b3fdf5e3c3acc213f6",
-  measurementId: "G-QX4MY946GD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
